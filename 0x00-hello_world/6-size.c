d361b6a24b0424c52b56612f205049bcfa5a435e
austynomilan <austynomilano77@gmail.com>
2023-06-15 19:33:36 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,7 +8,7 @@ int main(void)
 	 printf("Size of int: %zu bytes\n", sizeof(int));
 	  printf("Size of float: %zu bytes\n", sizeof(float));
 	   printf("Size of double: %zu bytes\n", sizeof(double));
-	   printf("Size of char: %zu byte\n", sizeof(char))
+	   printf("Size of char: %zu byte\n", sizeof(char));
 	return (0);
 }
 
