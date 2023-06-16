ed7ce576e01628dd4ccc5d347507c9e943195991
austynomilan <austynomilano77@gmail.com>
2023-06-15 21:12:36 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ *main- Entry point
+ *Return: Always 1 (success), otherwise a non unity
+ */
+int main(void)
+{
+	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19")
+		return (1)
+}
