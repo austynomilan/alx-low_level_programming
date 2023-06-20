f94dce2d4b25b5d15720fba494542bdedd055c8c
austynomilan <austynomilano77@gmail.com>
2023-06-19 18:09:36 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+  *main - Entry point
+  *Return: always 0
+  */
+int main(void)
+{
+	int y;
+
+	for (y = 0; y <= 9; y++)
+	{
+		printf("%d", y);
+	}
+	printf("\n");
+	return (0);
+}
