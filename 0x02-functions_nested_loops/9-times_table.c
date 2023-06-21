eee719bffa496738e5f740c35e2da776e0bb1abb
austynomilan <austynomilano77@gmail.com>
2023-06-20 20:41:51 -0700

    9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+  *times_table - entry point
+  *Return: ...
+  */
+void times_table(void)
+{
+	int x, y, z, u, d;
+
+	for (x = 0; x <= 9; x++)
+	{
+		for (y = 0; y <= 9; y++)
+		{
+			z = x * y;
+			if (z > 9)
+			{
+				u = z % 10;
+				d = (z - u) / 10;
+				_putchar(44);
+					_putchar(32);
+					_putcahr(32);
+			}
+			_putchar(z + '0);
+		}
+	}
+	_putchar('\n');
+}
