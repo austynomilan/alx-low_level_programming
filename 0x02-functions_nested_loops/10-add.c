01d0a6b950d2abc5651f073b90d0dcab8382a811
austynomilan <austynomilano77@gmail.com>
2023-06-20 20:56:24 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+  *add - Entry point
+  *@x: the first number to be computed
+  *@y: the second number to be computed
+  *Return: integer value
+  */
+
+int add(int x, int y)
+{
+	return (x + y);
+}
