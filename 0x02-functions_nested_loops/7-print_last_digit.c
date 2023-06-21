1a9c8717dc2dae2bc82a3b320bb385f0c95a381f
austynomilan <austynomilano77@gmail.com>
2023-06-20 20:00:01 -0700

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,21 @@
+#include "main.c"
+/**
+  *print_last_digit - Entry point
+  *@n: the number to be processed
+  *Return: value of the last digit
+  */
+
+int print_last_digit(int n)
+{
+	int last_digit;
+
+	last_digit = n % 10;
+	if (last_digit < 0)
+	{
+		last_digit = last_digit * -1;
+	}
+	_putchar(last_digit + '0');
+	return (last_digit);
+}
+
+
