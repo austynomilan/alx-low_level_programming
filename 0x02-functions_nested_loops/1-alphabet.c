fcd700da500930757f916719b24850c7efc3d54b
austynomilan <austynomilano77@gmail.com>
2023-06-20 17:34:32 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+  *print_letter - Entry point 
+  *Return: Always 0
+  */
+void print_letter(void)
+{
+	char x;
+
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		_putchar(x);
+	}
+	_putchar('\n')
+}
