4adaea58fef3115bf7e9da5c53fd977a0644b498
austynomilan <austynomilano77@gmail.com>
2023-06-22 04:26:18 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+  *mul - main entry
+  *@a: the first parameter
+  *@b: the second parametr
+  *Return: Always 0
+  */
+int mul(int a, int b)
+{
+	int multiply =  a * b;
+
+	return (multiply);
+}
