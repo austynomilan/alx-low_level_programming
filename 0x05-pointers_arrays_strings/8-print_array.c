efdaa096179627cb97a6081fa575ca597e03e8ce
austynomilan <austynomilano77@gmail.com>
2023-06-26 16:02:07 -0700

    8-print_array.c

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+/**
+  *print_array - Entry point
+  *@a: the pointer parameter
+  *@n: the integer number parameter
+  *Return: ...
+  */
+void print_array(int *a, int n)
+{
+	int indexOfArr;
+
+	for (indexOfArr = 0; indexOfArr < n; indexOfArr++)
+	{
+		printf("%d", a[indexOfArr]);
+		if (indexOfArr != (n -1))
+		{
+			printf(", ");
+		}
+	}
+	putchar('\n');
+}
+
+
