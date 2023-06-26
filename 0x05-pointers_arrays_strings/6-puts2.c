a0690c19a03ce0b1b3f23d86e9e75719ffae6875
austynomilan <austynomilano77@gmail.com>
2023-06-26 15:22:26 -0700

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+  *puts2 - Entry point
+  *@str: the string to be analysed
+  *Return: void
+  */
+void puts2(char *str)
+{
+	int z = 0;
+
+	while (str[z]!= '\0')
+	{
+		if (z % 2 == 0)
+		{
+			_putchar(str[z]);
+		}
+	}
+	_putchar('\n');
+}
+
