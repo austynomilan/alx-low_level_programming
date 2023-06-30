1ecf91ba9da43bed1ba7444d2341d6e02e3d1f7f
austynomilan <austynomilano77@gmail.com>
2023-06-29 18:01:39 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -13,22 +13,21 @@ char *cap_string(char *str)
 	while (str[index])
 	{
 		while (!(str[index] >= 97 && str[index] <= 122))
-				index++;
-
+			index++;
 		if (str[index - 1] == ' ' ||
 			str[index - 1] == '\t' ||
 			str[index - 1] == '\n' ||
 			str[index - 1] == ',' ||
-			 str[index - 1] == ';' ||
-			  str[index - 1] == '.' ||
-			  str[index - 1] == '!' ||
-			  str[index - 1] == '?' ||
-			  str[index - 1] == '"' ||
-			  str[index - 1] == '(' ||
-			  str[index - 1] == ')' ||
-			  str[index - 1] == '{' ||
-			  str[index - 1] == '}' ||
-			  index == 0)
+			str[index - 1] == ';' ||
+			str[index - 1] == '.' ||
+			str[index - 1] == '!' ||
+			str[index - 1] == '?' ||
+			str[index - 1] == '"' ||
+			str[index - 1] == '(' ||
+			str[index - 1] == ')' ||
+			str[index - 1] == '{' ||
+			str[index - 1] == '}' ||
+			index == 0)
 		str[index] -= 32;
 
 		index++;
