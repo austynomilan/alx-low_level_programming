3f336cecb706598472e40a2b97afb8bf7cdf05a5
austynomilan <austynomilano77@gmail.com>
2023-07-03 12:51:20 -0700

    5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -10,9 +10,9 @@ char *_strstr(char *haystack, char *needle)
 	for (; *haystack != '\0'; haystack++)
 	{
 		char *l = haystack;
-		char *p = neddle;
+		char *p = needle;
 
-		while (*l  == *p && != '\0')
+		while (*l == *p && *p != '\0')
 		{
 			l++;
 			p++;
