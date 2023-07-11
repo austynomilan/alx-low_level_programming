2355673ada8f6c0b4041a7c20d03b2006b331804
austynomilan <austynomilano77@gmail.com>
2023-07-11 02:07:17 -0700

    2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -12,12 +12,10 @@ int main(int argc, char *argv[])
 {
 	int i;
 
-	if (i > 1)
+	for (i = 0; i < argc; i++)
 	{
-		for (i = 1; i < argc; i++)
-		{
-			printf("%s\n", argv[i]);
-		}
+		printf("%s\n", argv[i]);
 	}
+
 	return (0);
 }
