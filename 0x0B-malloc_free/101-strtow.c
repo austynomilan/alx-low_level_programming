9c46eddbab7df9f329866cd0c6885bb29c4153c6
austynomilan <austynomilano77@gmail.com>
2023-07-11 17:21:57 -0700

    101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -33,12 +33,12 @@ int count_word(char *s)
  */
 char **strtow(char *str)
 {
-	char **matrix, *tmp:
+	char **matrix, *tmp;
 	int i, k = 0, len = 0, words, c = 0, start, end;
 
 	while (*(str + len))
 		len++;
-	words = count_words(str);
+	words = count_word(str);
 	if (words == 0)
 	{
 		return (NULL);
@@ -54,7 +54,7 @@ char **strtow(char *str)
 			if (c)
 			{
 				end = i;
-				tmp = (char *) malloc(sizeof(char) * (c + 1);
+				tmp = (char *) malloc(sizeof(char) * (c + 1));
 				if (tmp == NULL)
 					return (NULL);
 				while (start < end)
