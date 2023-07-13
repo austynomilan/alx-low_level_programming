1d9a1b62ef46b1db321f23039a138a0e7a106894
austynomilan <austynomilano77@gmail.com>
2023-07-13 16:55:47 -0700

    0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -10,11 +10,13 @@
  */
 void *malloc_checked(unsigned int b)
 {
-	void *ptr = malloc(b);
+	void *ptr;
+
+	ptr= malloc(b);
+
 	if (ptr == NULL)
 	{
-		printf("failed to allocate memory. \n");
 		exit(98);
 	}
-	return ptr;
+	return (ptr);
 }
