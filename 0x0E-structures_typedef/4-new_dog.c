b1bbd5dea8c18779936646f0be08e7e895161ac6
austynomilan <austynomilano77@gmail.com>
2023-07-17 17:34:03 -0700

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "dog.h"
 /**
  * new_dog - creates a new dog
  * @name: name of the dog
@@ -10,16 +11,9 @@
  * Return: pointer to the new dog (Success), NULL otherwise
  */
 
-typedef struct
-{
-	char *name;
-	float age;
-	char *owner;
-} dog_t;
-
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t* dog;
+	dog_t *dog;
 	int len1, len2;
 
 	len1 = strlen(name);
