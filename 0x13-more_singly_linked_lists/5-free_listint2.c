bc5cf96813cdb13774336c7a3030c0d0254b63b9
austynomilan <austynomilano77@gmail.com>
2023-08-01 17:24:32 -0700

    rev

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,10 +8,14 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp;
 
-	while (*head != NULL)
+	if (head == NULL)
+		return;
+	while (*head)
 	{
-		temp = *head;
-		*head = (*head)->next;
-		free(temp);
+		temp = (*head)->next;
+		free(*head);
+		*head = temp;
 	}
+
+	*head = NULL;
 }
