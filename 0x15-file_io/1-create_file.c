e8a190631b3725afa7bb1a0f913ee398d53a1773
austynomilan <austynomilano77@gmail.com>
2023-08-08 02:18:56 -0700

    ceate a file func

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+
+
+/**
+ * create_file - Creates a file.
+ * @filename: A pointer to the name of the file to create.
+ * @text_content: A pointer to a string to write to the file.
+ *
+ * Return: If the function fails - -1.
+ *         Otherwise - 1.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+
+	if (text_content != NULL)
+	{
+		ssize_t bytes_written = write(fd, text_content, strlen(text_content));
+
+		if (bytes_written == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+	close(fd);
+	return(1);
+}
