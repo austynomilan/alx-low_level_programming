21e266e90250102b4efcb6bf03262d16b319649a
Augustine <austynomilano77@gmail.com>
2023-09-04 07:09:43 -0700

    updated create file func

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,9 +14,8 @@ int create_file(const char *filename, char *text_content)
 	int nletters;
 	int rwr;
 
-	if(!filename)
+	if (!filename)
 		return (-1);
-	
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
 	if (fd == -1)
@@ -35,6 +34,6 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	}
 	close(fd);
-	
+
 	return (1);
 }
